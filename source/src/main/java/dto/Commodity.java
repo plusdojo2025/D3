11a38fdaa9328aaf2a0a6d0b501bc826e4c398e8
plusdojo2025-yamazaki <yamazaki-kanoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 15:16:59 +0900

    Update Commodity.java

--- source/src/main/java/dto/Commodity.java
+++ source/src/main/java/dto/Commodity.java
@@ -11,76 +11,6 @@ public class Commodity implements Serializable{
 	private String commodity_image;	/*イメージ*/
 	
 	
-	
-	public int getCommodity_id() {
-		return commodity_id;
-	}
-
-
-
-
-	public void setCommodity_id(int commodity_id) {
-		this.commodity_id = commodity_id;
-	}
-
-
-
-
-	public String getCommodity_name() {
-		return commodity_name;
-	}
-
-
-
-
-	public void setCommodity_name(String commodity_name) {
-		this.commodity_name = commodity_name;
-	}
-
-
-
-
-	public int getCommodity_price() {
-		return commodity_price;
-	}
-
-
-
-
-	public void setCommodity_price(int commodity_price) {
-		this.commodity_price = commodity_price;
-	}
-
-
-
-
-	public int getCommodity_category() {
-		return commodity_category;
-	}
-
-
-
-
-	public void setCommodity_category(int commodity_category) {
-		this.commodity_category = commodity_category;
-	}
-
-
-
-
-	public String getCommodity_image() {
-		return commodity_image;
-	}
-
-
-
-
-	public void setCommodity_image(String commodity_image) {
-		this.commodity_image = commodity_image;
-	}
-
-
-
 
 	public Commodity(int commodity_id, String commodity_name, int commodity_price, int commodity_category,
 			String commodity_image) {
@@ -93,5 +23,43 @@ public class Commodity implements Serializable{
 	}
 	
 	
-	
+	public int getCommodity_id() {
+		return commodity_id;
+	}
+
+	public void setCommodity_id(int commodity_id) {
+		this.commodity_id = commodity_id;
+	}
+
+	public String getCommodity_name() {
+		return commodity_name;
+	}
+
+	public void setCommodity_name(String commodity_name) {
+		this.commodity_name = commodity_name;
+	}
+
+	public int getCommodity_price() {
+		return commodity_price;
+	}
+
+	public void setCommodity_price(int commodity_price) {
+		this.commodity_price = commodity_price;
+	}
+
+	public int getCommodity_category() {
+		return commodity_category;
+	}
+
+	public void setCommodity_category(int commodity_category) {
+		this.commodity_category = commodity_category;
+	}
+
+	public String getCommodity_image() {
+		return commodity_image;
+	}
+
+	public void setCommodity_image(String commodity_image) {
+		this.commodity_image = commodity_image;
+	}
 }
