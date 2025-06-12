f2113800338e3c8802533169f16730abc83e6292
plusdojo2025-watanabe-miku <watanabe-miku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 13:47:52 +0900

    Update Event.java

--- source/src/main/java/dto/Event.java
+++ source/src/main/java/dto/Event.java
@@ -1,5 +1,12 @@
 package dto;
 
-public class Event {
+import java.io.Serializable;
 
+public class Event implements Serializable{
+
+	private int event_id;//イベントID
+	private int store_id;//店舗ID
+	private String event_date;//日付
+	private String event_name;//イベント名
+	private String event_remark;//備考
 }
