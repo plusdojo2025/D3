eaef67732f8aff6b6a039cd68f02fcb5a45ed174
plusdojo2025-watanabe-miku <watanabe-miku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 16:09:28 +0900

    追加

--- source/src/main/java/servlet/OrderSubmitServlet.java
+++ source/src/main/java/servlet/OrderSubmitServlet.java
@@ -1,10 +1,66 @@
 package servlet;
+	
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class OrderSubmitServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import dto.OrderList;
 
+
+/**
+* Servlet implementation class LoginServlet
+*/
+@WebServlet("/OrderSubmitServlet")
+public class OrderSubmitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	*      response)
+	*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		
+		
 	}
 
-}
+	/**
+	* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	*      response)
+	*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String company = request.getParameter("company");
+				String companysub = request.getParameter("companysub");
+				
+				
+
+		
+		
+			List<OrderList> list= new ArrayList<OrderList>();
+			for(int i=0;i<5;i++) {
+				//受け取ったメニューデータをセッションで送る
+				list.add(new OrderList(0,1,null,3));
+			}
+			
+			// 検索結果をリクエストスコープに格納する
+			request.setAttribute("orderList", list);
+			
+
+			// OrderListServletにリダイレクトする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/OrderListServlet");
+			dispatcher.forward(request, response);
+		}
+	}
+	
