18e229c82f696b3e48dc71847f5a4a16aa2665f3
plusdojo2025-watanabe-miku <watanabe-miku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 10:58:42 +0900

    追加

--- source/src/main/java/test/EventDAOTest.java
+++ source/src/main/java/test/EventDAOTest.java
@@ -1,10 +1,38 @@
 package test;
 
+import java.util.List;
+
+import dao.EventDAO;
+import dto.Event;
+
 public class EventDAOTest {
-
+	
+	public static void showAllData(List<Event> eventList) {
+		for (Event event : eventList) {
+			
+			System.out.println("イベントID：" + event.getEvent_id());
+			System.out.println("店舗ID：" + event.getStore_id());
+			System.out.println("日付：" + event.getEvent_date());
+			System.out.println("イベント名：" + event.getEvent_name());
+			System.out.println("備考：" + event.getEvent_remark());
+			System.out.println();
+		
+		}
+			
+			
+	}
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		EventDAO dao = new EventDAO();
 
+		// select()のテスト
+		System.out.println("---------- select()のテスト1 ----------");
+		List<Event> cardListSel1 = dao.select();
+		EventDAOTest.showAllData(cardListSel1);
+		
+		// insert(Event event)のテスト
+		System.out.println("---------- insert(Event event)のテスト1 ----------");
+		boolean cardListSel2 = dao.insert(new Event(0,0,"","ハロウィン","仮装したらドリンクプレゼント"));
+		System.out.println(cardListSel2);
 	}
 
 }
