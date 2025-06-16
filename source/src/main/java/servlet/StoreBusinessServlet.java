2d6cb8a83b7f58362035f0f57654b6f907ff4676
plusdojo2025-watanabe-miku <watanabe-miku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 08:42:03 +0900

    更新

--- source/src/main/java/servlet/StoreBusinessServlet.java
+++ source/src/main/java/servlet/StoreBusinessServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 	
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.OrderListDAO;
+import dto.OrderList;
 
 
 /**
@@ -23,10 +27,13 @@ public class StoreBusinessServlet extends HttpServlet {
 	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
+		OrderListDAO dao = new OrderListDAO();
+		List<OrderList> orderData = dao.select_new(new OrderList(-1,-1,"",-1));
+//		
 		
-		request.setAttribute("連絡事項", data);
-		request.setAttribute("注文商品", data);
-		request.setAttribute("来店者", data);
+		//request.setAttribute("連絡事項", data1);
+		request.setAttribute("orderData", orderData);
+		//request.setAttribute("来店者", data3);
 		
 		
 
