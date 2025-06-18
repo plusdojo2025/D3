39492923880be8f7f1dceec0e33b99de0a376ac3
plusdojo2025-yamazaki <yamazaki-kanoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 15:10:04 +0900

    変更

--- source/src/main/java/servlet/StoreStaffServlet.java
+++ source/src/main/java/servlet/StoreStaffServlet.java
@@ -1,10 +1,47 @@
 package servlet;
 
-public class StoreStaffServlet {
+import java.io.IOException;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import dao.EventDAO;
+import dto.Event;
+
+@WebServlet("/StoreStaffServlet")
+public class StoreStaffServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+	//イベント告知
+		//リクエストパラメータ取得
+		request.setCharacterEncoding("UTF-8");
+		String eventdate = request.getParameter("event_date");
+		String eventname = request.getParameter("event_name");
+		String eventremark = request.getParameter("event_remark");
+		
+		EventDAO eventDao = new EventDAO();
+		boolean ins1 = eventDao.insert(new Event(0,0,eventdate,eventname,eventremark));
+		if(ins1==true) {
+			List<Event>sel = eventDao.select();
+			request.setAttribute("eventList",("新しいイベントを登録しました。"));
+			eventDao.showAllData(sel);
+		}else {
+			
+		}
+		
+		
+	//顧客一覧
+		
+		
+	//注文履歴
 	}
 
 }
+
