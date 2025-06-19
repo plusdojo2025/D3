fac231143eb1ae564f9ba399337962e7600ea6ab
plusdojo2025-yamazaki <yamazaki-kanoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 11:59:17 +0900

    Update StoreStaffServlet.java

--- source/src/main/java/servlet/StoreStaffServlet.java
+++ source/src/main/java/servlet/StoreStaffServlet.java
@@ -19,7 +19,7 @@ import dto.StoreMemo;
 public class StoreStaffServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 	request.setCharacterEncoding("UTF-8");
@@ -34,6 +34,7 @@ public class StoreStaffServlet extends HttpServlet {
 		
 		EventDAO eventDao = new EventDAO();
 		boolean ins = eventDao.insert(new Event(0,0,eventdate,eventname,eventremark));
+		
 		if(ins==true) {
 			List<Event>sel = eventDao.select();
 			request.setAttribute("eventList",sel);
@@ -41,25 +42,35 @@ public class StoreStaffServlet extends HttpServlet {
 		}else {
 		    request.setAttribute("message", "イベントの登録に失敗しました。");
 		}}
-		else if ("insertMemo".equals(action)) {
-
+		
 		//業務連絡登録
+		else if ("insertMemo".equals(action)) {
 		String storedate = request.getParameter("store_date");
 		String storeremark = request.getParameter("store_remark");
 		
-		StoreMemoDAO storemDao = new StoreMemoDAO();
-		boolean ins1 = storemDao.insert(new StoreMemo(0,storedate,storeremark));
-		if(ins1==true) {
-			List<StoreMemo>sel1 = storemDao.select();
-			request.setAttribute("memoList", sel1());
-		}}
-	//顧客一覧		
+		StoreMemoDAO storememoDao = new StoreMemoDAO();
+		boolean ins1 = storememoDao.insert(new StoreMemo(0,storedate,storeremark));
 		
+		if (ins1) {
+			// 空のStoreMemoを渡して全件取得（DAO変更なしで実現）
+			List<StoreMemo> memoList = storememoDao.select(new StoreMemo(0, "", ""));
+			request.setAttribute("storememoList", memoList);
+			request.setAttribute("message", "業務連絡を登録しました。");
+		} else {
+			request.setAttribute("message", "業務連絡の登録に失敗しました。");
+		}}
+		
+		
+	//顧客一覧		
+		else {
+			
+		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/StoreStaff.jsp");
 		dispatcher.forward(request,response);
 	}
+}
 
 	
 		
-}
+
