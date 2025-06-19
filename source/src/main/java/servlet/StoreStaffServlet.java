405ca403354b45237781c7f255012cd71f060311
plusdojo2025-minoda <minoda-yusaku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 13:43:12 +0900

    衝突解消

--- source/src/main/java/servlet/StoreStaffServlet.java
+++ source/src/main/java/servlet/StoreStaffServlet.java
@@ -27,144 +27,96 @@ public class StoreStaffServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		
-		
-		
-		//来店者一覧
-		
+		// 来店者一覧
+
 		Date today = Date.valueOf(LocalDate.now());
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String dateString = formatter.format(today);
-        
-        // TODO 現時点注文済みの客の情報のみ表示
-     	VisitorDAO dao = new VisitorDAO();
-     	List<Visitor> visitor = dao.getVisitorByDate(dateString);
+		String dateString = formatter.format(today);
 
-     	request.setAttribute("visitor", visitor);
-		
-	
-		
+		// TODO 現時点注文済みの客の情報のみ表示
+		VisitorDAO dao = new VisitorDAO();
+		List<Visitor> visitor = dao.getVisitorByDate(dateString);
+
+		request.setAttribute("visitor", visitor);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/StoreStaff.jsp");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		//イベント登録
-<<<<<<< HEAD
-		
+		request.setCharacterEncoding("UTF-8");
+
+		// e_actionリクエストパラメータを取得する（insert,update,deleteを区別する）
+		// イベント登録
+		String message = "";
+
 		String eventdate = request.getParameter("event_date");
 		String eventname = request.getParameter("event_name");
 		String eventremark = request.getParameter("event_remark");
-		
-		EventDAO eventDao = new EventDAO();
-		boolean ins = eventDao.insert(new Event(0,0,eventdate,eventname,eventremark));
-		
-		if(ins==true) {
-			List<Event>sel = eventDao.select();
-			request.setAttribute("eventList",sel);
-			request.setAttribute("message", "イベントを登録しました。");
-		}else {
-		    request.setAttribute("message", "イベントの登録に失敗しました。");
-		}}
-		
-		//業務連絡登録
-		else if ("insertMemo".equals(action)) {
+
+		if (eventdate != null && !eventdate.equals("") && !eventname.equals("") && !eventremark.equals("")) {
+			EventDAO eventDao = new EventDAO();
+			boolean ins = eventDao.insert(new Event(0, 1, eventdate, eventname, eventremark));
+			if (ins == true) {
+
+				request.setAttribute("eventList", ("新しいイベントを登録しました。"));
+
+				message = ("新しいイベントを登録しました。");
+
+			} else {
+				message = "イベントの登録に失敗しました。";
+			}
+			System.out.println(eventdate);
+			System.out.println(message);
+		}
+		// イベント更新
+		/*
+		 * if (request.getParameter("update").equals("更新")) { if(eventDao.update(new
+		 * Event(0, 0, eventdate,eventname,eventremark))) { message =
+		 * ("イベント内容を更新しました。"); } }
+		 */
+
+		// 業務連絡登録
 		String storedate = request.getParameter("store_date");
 		String storeremark = request.getParameter("store_remark");
-		
-		StoreMemoDAO storememoDao = new StoreMemoDAO();
-		boolean ins1 = storememoDao.insert(new StoreMemo(0,storedate,storeremark));
-=======
-				request.setCharacterEncoding("UTF-8");
-				//e_actionリクエストパラメータを取得する（insert,update,deleteを区別する）
-				String message ="";
-				
-				String eventdate = request.getParameter("event_date");
-				String eventname = request.getParameter("event_name");
-				String eventremark = request.getParameter("event_remark");
-				
-				if(eventdate != null && !eventdate.equals("") && !eventname.equals("") && !eventremark.equals("")) {
-					EventDAO eventDao = new EventDAO();
-					boolean ins = eventDao.insert(new Event(0,1,eventdate,eventname,eventremark));
-					if(ins==true) {
 
-						request.setAttribute("eventList",("新しいイベントを登録しました。"));
+		if (storedate != null && !storedate.equals("") && !storeremark.equals("")) {
+			StoreMemoDAO storemDao = new StoreMemoDAO();
+			boolean ins1 = storemDao.insert(new StoreMemo(0, storedate, storeremark));
+			if (ins1 == true) {
+				message = ("新しい業務連絡を登録しました。");
+			} else {
+				message = "業務連絡の登録に失敗しました。";
+			}
 
-						message = ("新しいイベントを登録しました。");
+			if (ins1) {
+				// 空のStoreMemoを渡して全件取得（DAO変更なしで実現）
+				List<StoreMemo> memoList = storemDao.select(new StoreMemo(0, "", ""));
+				request.setAttribute("storememoList", memoList);
+				request.setAttribute("message", "業務連絡を登録しました。");
+			} else {
+				request.setAttribute("message", "業務連絡の登録に失敗しました。");
+			}
 
-					}
-					else {
-						message = "イベントの登録に失敗しました。";	
-					}
-					System.out.println(eventdate);
-					System.out.println(message);
-				}
-				//イベント更新
-				/*if (request.getParameter("update").equals("更新")) {
-					if(eventDao.update(new Event(0, 0, eventdate,eventname,eventremark))) {
-						message = ("イベント内容を更新しました。");
-					}
-				}*/
-
-				
-				//業務連絡登録
-				String storedate = request.getParameter("store_date");
-				String storeremark = request.getParameter("store_remark");
-				
-				if(storedate!=null && !storedate.equals("") && !storeremark.equals("")) {
-					StoreMemoDAO storemDao = new StoreMemoDAO();
-					boolean ins1 = storemDao.insert(new StoreMemo(0,storedate,storeremark));
-					if(ins1==true) {
-						message = ("新しい業務連絡を登録しました。");
-					}
-					else {
-						message = "業務連絡の登録に失敗しました。";	
-					}
-					System.out.println(storedate);
-					System.out.println(message);
-				}
-				
-				//来店者一覧
-				
-				Date today = Date.valueOf(LocalDate.now());
-				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		        String dateString = formatter.format(today);
-		        
-		        // TODO 現時点注文済みの客の情報のみ表示
-		     	VisitorDAO dao = new VisitorDAO();
-		     	List<Visitor> visitor = dao.getVisitorByDate(dateString);
-
-		     	request.setAttribute("visitor", visitor);
->>>>>>> e147dce9684f52f6777b3aa2feb500a89e99efa1
-		
-		if (ins1) {
-			// 空のStoreMemoを渡して全件取得（DAO変更なしで実現）
-			List<StoreMemo> memoList = storememoDao.select(new StoreMemo(0, "", ""));
-			request.setAttribute("storememoList", memoList);
-			request.setAttribute("message", "業務連絡を登録しました。");
-		} else {
-			request.setAttribute("message", "業務連絡の登録に失敗しました。");
-		}}
-		
-		
-	//顧客一覧		
-		else {
-			
+			System.out.println(storedate);
+			System.out.println(message);
 		}
+
+		// 来店者一覧
+		Date today = Date.valueOf(LocalDate.now());
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String dateString = formatter.format(today);
+
+		// TODO 現時点注文済みの客の情報のみ表示
+		VisitorDAO dao = new VisitorDAO();
+		List<Visitor> visitor = dao.getVisitorByDate(dateString);
 		
+		
+
+		request.setAttribute("visitor", visitor);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/StoreStaff.jsp");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 	}
 }
-
-	
-<<<<<<< HEAD
-		
-
-=======
-	
-	
-}
->>>>>>> e147dce9684f52f6777b3aa2feb500a89e99efa1
