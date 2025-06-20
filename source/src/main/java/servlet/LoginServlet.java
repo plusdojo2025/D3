1231dfd1dcf4835547c8b20b82ec456d311938a7
plusdojo2025-minoda <minoda-yusaku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 17:37:37 +0900

    店舗ログイン後業務画面に遷移

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -50,9 +50,7 @@ public class LoginServlet extends HttpServlet {
 			Store store = new StoreDAO().login(email, password);
 			if (store != null) {
 				session.setAttribute("store", store);
-				 //response.sendRedirect("/WEB-INF/jsp/CustomerHome.jsp");
-                RequestDispatcher dispatcher = request.getRequestDispatcher("/CustomerHomeServlet");
-    			dispatcher.forward(request, response);
+				 response.sendRedirect(request.getContextPath() + "/StoreBusinessServlet");
 			} else {
 				request.setAttribute("errorMsg", "店舗ログイン失敗。メールアドレスまたはパスワードが間違っています。");
 				request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp").forward(request, response);
